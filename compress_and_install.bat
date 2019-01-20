d68047e5fc788b49a2e23c4bb300489989f1da63
Temur <tvibliani@users.noreply.github.com>
2019-01-21 02:17:35 +0400

    Install straight away

---
+++ compress_and_install.bat
@@ -0,0 +1,22 @@
+@echo off
+
+SET pwd=%~dp0
+SET libDestination=%pwd%CUDA_10.0_Library_example.zip
+SET appDestination=%pwd%CUDA_10.0_Application_example.zip
+
+FOR /F "tokens=3" %%G IN ('REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Personal"') DO (SET docsdir=%%G)
+echo "MyDocuments: %docsdir%"
+
+SET install_dir=%docsdir%\Visual Studio %VS_VERSION%\Templates\ProjectTemplates
+SET source_dir=%docsdir%\Visual Studio %VS_VERSION%\My Exported Templates
+echo "Source  Directory: %source_dir%"
+echo "Install Directory: %install_dir%"
+
+if exist "%libDestination%" del "%libDestination%"
+powershell Compress-Archive -Path %pwd%CUDA_10.0_Library_example/* -Destination "%libDestination%"
+
+if exist "%appDestination%" del "%appDestination%"
+powershell Compress-Archive -Path %pwd%CUDA_10.0_Application_example/* -Destination "%appDestination%"
+
+robocopy "%pwd% "  "%install_dir%"  "*.zip" /V
+robocopy "%pwd% "  "%source_dir%"  "*.zip" /V /MOV
